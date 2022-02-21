2bfbc29ada5e26c01f14c34c892c35c20ecd62a0
Alexandr Zagumennov <zagumennov.alex@yandex.ru>
2022-02-21 16:10:19 +0300

    Секция BOOKING-FORM: откорректировано отображение календаря, списка времени, отображение полей на малых разрешениях, выбранные чекбоксы

--- js/flatpickr.js
+++ js/flatpickr.js
@@ -1,3 +1,13 @@
+const bookingFormData = document.getElementById('booking-form__data');
+
 flatpickr(".booking-form__data", {
-	"locale": "ru"
-});
+	dateFormat: "d.m.",
+	"locale": "ru",
+	onClose: function(){
+		bookingFormData.classList.add('visibil')
+	 }
+	}
+);
+
+
+
